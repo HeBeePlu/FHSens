3aa52d4613f6bd14c95e8ded12c0007bc252ed6e
HeBeePlu <hendrikbeecken@aol.com>
2019-01-07 15:06:34 +0100

    Script zum Publishen

--- MQTTBroker.py
+++ MQTTBroker.py
@@ -4,3 +4,14 @@
 # MQTT Broker zur Kommunikation der Sensordaten
 
 
+import paho.mqtt.client as mqtt
+
+broker_adress = "192.168.178.45"
+
+client = mqtt.Client("Publisher_1")
+
+#client.connect("localhost", 1883, 60)
+client.connect(broker_adress)
+
+#publish Data
+client.publish("fun/topic","Py Script Hallo")
