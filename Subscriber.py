66d92532ffe8c79c8569f23b4e43d432d90b619f
HeBeePlu <hendrikbeecken@aol.com>
2019-01-07 16:39:16 +0100

    Subscriber erstellt

---
+++ Subscriber.py
@@ -0,0 +1,19 @@
+#Hendrik Beecken
+#
+#07.01.2019
+#
+#Subscriber-Script
+
+
+
+import paho.mqtt.client as mqtt
+
+broker_adress = "192.168.178.45"
+
+client = mqtt.Client("Subscriber_1")
+
+#client.connect("localhost", 1883, 60) #alternative zur ip
+client.connect(broker_adress)
+
+#Topic "topic" wird abonniert
+client.subscribe("topic")
