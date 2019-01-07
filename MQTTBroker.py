e15b3bbb8b171a63d23bdcac2868ac78645b1a18
HeBeePlu <hendrikbeecken@aol.com>
2019-01-07 15:28:36 +0100

    Brokerstart implementiert

--- MQTTBroker.py
+++ MQTTBroker.py
@@ -3,8 +3,10 @@
 #
 # MQTT Broker zur Kommunikation der Sensordaten
 
-
+import os
 import paho.mqtt.client as mqtt
 
 #broker_adress = "192.168.178.45"
 
+# Startet mosquitto Broker auf Port 1883 im Hintergrund
+os.system('mosquitto -p 1883 -d')
