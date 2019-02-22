d7ce0d28121babe80caf38b6055620bab553b888
HeBeePlu <hendrikbeecken@aol.com>
2019-02-22 15:13:21 +0100

    fehler behoben

--- UDPSensorServer.py
+++ UDPSensorServer.py
@@ -5,7 +5,7 @@ import socket
 UDP_IP_ADDRESS = "192.168.178.45"
 UDP_PORT_NO = 49200
 
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_DRAGM)
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 
