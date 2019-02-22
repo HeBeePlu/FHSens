216cde82f4f2cebe3c1420e0c11ab061c28e20dc
HeBeePlu <hendrikbeecken@aol.com>
2019-02-22 15:10:05 +0100

    UDP Server funktion geschrieben

--- UDPSensorServer.py
+++ UDPSensorServer.py
@@ -1,2 +1,15 @@
 # UDP server zur annahme der sensordten
 
+import socket
+
+UDP_IP_ADDRESS = "192.168.178.45"
+UDP_PORT_NO = 49200
+
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_DRAGM)
+
+serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+
+
+while True:
+    data, addr = serverSock.recvfrom(1024)
+    print("Message: ", data)
