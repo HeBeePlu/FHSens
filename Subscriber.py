227049dc68525f3b10c1ba3e23c0b27baec7c3ac
HeBeePlu <hendrikbeecken@aol.com>
2019-03-19 11:20:41 +0100

    Registermessage subscriber

--- Subscriber.py
+++ Subscriber.py
@@ -12,8 +12,6 @@ import ipadress
 
 broker_adress = "192.168.178.45"
 
-service_ip = ipadress.get_ip()
-
 client = mqtt.Client()#mqtt.Client("Client_Name") optional aber Name darf nur ein mal vergeben werden
 
 #client.connect("localhost", 1883, 60) #alternative zur ip
@@ -21,17 +19,32 @@ client.connect(broker_adress)
 
 #Topic "topic" wird abonniert mit einem Quality of Service qos
 client.subscribe("UDP-Sensor", 0)
+print('MQTT Client up')
 
+service_ip = ipadress.get_ip()
+service_name = 'Subscriber Service'
+service_status = True
 
-subscriber_service_ip = json.dumps(ipadress.get_ip())
-sensorClient.publish("ServiceRegister", subscriber_service_ip)
-print("Subscriber-Service IP: ", ipadress.get_ip())
+service_props = { 'ServiceName' : service_name,
+                  'Service IP' : service_ip,
+                  'Servicestatus' : service_status }
+
+register_msg = json.dumps(service_props)
+sensorClient.publish("ServiceRegister", register_msg)
+print ('UDPtoMQTT_Service IP: ', service_ip)
 
 def on_message(client, userdata, msg):
     msg_in = json.loads(msg.payload)
     print(msg.topic + " " + str(msg_in))
     
-    
-client.on_message = on_message
-
-client.loop_forever()
+def main():
+    try:
+        client.on_message = on_message
+        client.loop_forever()
+        
+    except:
+        service_status = False
+        service_props.update({'Servicestatus': service_status})
+        register_msg = json.dumps(service_props)
+        sensorClient.publish("ServiceRegister", register_msg)
+        print("Subscriber Service down")
