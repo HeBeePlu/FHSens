748acaa46be93ebf424e3d58289935c64423c806
HeBeePlu <hendrikbeecken@aol.com>
2019-03-19 10:09:13 +0100

    Registermessage

--- SensorDummy.py
+++ SensorDummy.py
@@ -9,21 +9,27 @@ UDP_IP_ADDRESS = "192.168.178.45"
 UDP_PORT_NO = 49200
 
 service_ip = ipadress.get_ip()
+service_name = 'Sensor Dummy'
+service_status = True
 
-msg = "Hallo Server"
 sleepTime = 0.012
 
 #erstellen eines Sockets zur Verbindung mit dem Server
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print('UDP Sensor socket up')
 
-udp_sensordummy_service_ip = json.dumps(ipadress.get_ip())
-sensorClient.publish("ServiceRegister", udp_sensordummy_service_ip)
-print ('UDP-Sensor IP: ', ipadress.get_ip())
+service_props = { 'ServiceName' : service_name,
+                  'Service IP' : service_ip,
+                  'Servicestatus' : service_status }
+    
+
+register_msg = json.dumps(service_props)
+sensorClient.publish("ServiceRegister", service_props)
+print ('UDP-Sensor IP: ', service_ip)
 #senden der Daten an den server
 def main():
     try:
-        while True:
+        while service_status = True:
             for msg in range(0, 256):
                 msg=str(msg)
                 clientSock.sendto(msg.encode('utf-8'), (UDP_IP_ADDRESS, UDP_PORT_NO))
@@ -34,6 +40,10 @@ def main():
                 clientSock.sendto(msg.encode('utf-8'), (UDP_IP_ADDRESS, UDP_PORT_NO))
                 time.sleep(sleepTime)
     except:
+        service_status = False
+        service_props.update({'Servicestatus': service_status})
+        register_msg = json.dumps(service_props)
+        sensorClient.publish("ServiceRegister", service_props)
         print("Sensor abgestürzt")
 
 if __name__=='__main__':
