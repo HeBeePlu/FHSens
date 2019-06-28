4b661d9194700184f244732080d76a64ebc9640f
HeBeePlu <hendrikbeecken@aol.com>
2019-06-28 13:38:53 +0200

    zeitformatter

--- Subscriber.py
+++ Subscriber.py
@@ -10,6 +10,7 @@ import paho.mqtt.client as mqtt
 import json
 import ipadress
 from datetime import datetime
+from datetime import timedelta
 
 broker_adress = "192.168.178.45" #ip des geraetes, auf dem der mqtt brocker laeuft
 
@@ -19,8 +20,8 @@ client = mqtt.Client()#mqtt.Client("Client_Name") optional aber Name darf nur ei
 client.connect(broker_adress)
 
 #alle topics ab UDP-Sensor/ werden abonniert mit einem Quality of Service qos
-#client.subscribe("UDP-Sensor/#", 0)
-client.subscribe("Data-Log", 0)
+client.subscribe("UDP-Sensor/#", 0)
+#client.subscribe("Data-Log", 0)
 print('MQTT Client up')
 
 service_ip = ipadress.get_ip()
@@ -28,13 +29,23 @@ service_name = 'Subscriber Service'
 
 print ('Subscriber_Service IP: ', service_ip)
 
-
+def zeitformat (zeit):
+    zeitList = list()
+    for n in range(6, 14):
+        zeitList.append(zeit[n])
+    zeitString = ''.join(str(i) for i in zeitList)
+    return zeitString        
+            
 #funktion zum auslesen des mqtt topics
 def on_message(client, userdata, msg):
     msg_in = json.loads(msg.payload) #json daten entpacken
     timeStamp = str(datetime.now().time()) #zeitstempel einfuegen
     msg_in.update({'Subscriber Zeit' : timeStamp})
-    print(msg_in)
+    starttime = str(msg_in['Zeit udp2mqtt'])
+    
+    a = zeitformat(starttime)
+    
+    print(a)
     
 def main():
     try:
